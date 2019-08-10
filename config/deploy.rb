85b7177f456c73a9381e36533dc6e94d30829846
Daniel Podvseker <daniel.podvesker@builderpod.co>
2019-08-10 20:01:40 +0000

    Se

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:builderpod/builder_pod.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/secrets.yml'
+append :linked_files, "config/database.yml", 'config/credentials.yml.enc'
 
 # Default value for linked_dirs is []
  append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
