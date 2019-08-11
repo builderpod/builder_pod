69e1e00c34fb7f8502f16733d8039215eb5a3a3a
Daniel Podvseker <daniel.podvesker@builderpod.co>
2019-08-11 08:31:00 +0000

    cats

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,9 +4,9 @@ class Category < ApplicationRecord
   def self.format_json
     json_arr = [];
     Category.order(:name).includes(:task_profiles).each do |c|
-      json_arr << ["key": c.id, "section": true, "label": c.name]
+      json_arr << {"key": c.id, "section": true, "label": c.name}
       c.task_profiles.each {|t|
-        json_arr << ["key": t.id, "label": t.name]
+        json_arr << {"key": t.id, "label": t.name}
       }
     end
     return json_arr
